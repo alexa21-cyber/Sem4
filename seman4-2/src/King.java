e91f369f496beb5a58a6968ec3f1fc782e0c941e
zafiro <you@example.com>
2020-05-07 21:40:21 +0200

    primer

---
+++ seman4-2/src/King.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class King {
+	
+	public static void main(String[] args) {
+		int num;
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Ingese un numero");
+		num=scan.nextInt();
+	
+	}
+}
